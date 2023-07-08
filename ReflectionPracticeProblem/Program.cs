0dc49280dfacaef037612f3627465fc291641f80
rishiraj007 <rishiraj@mounttalent.com>
2023-07-08 16:17:04 +0530

    [Prabhu]Add: Reflection Practice Problem

---
+++ ReflectionPracticeProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReflectionPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reflection Practice Problem!");
+        }
+    }
+}
