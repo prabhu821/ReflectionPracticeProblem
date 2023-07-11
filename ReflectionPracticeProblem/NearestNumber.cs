6a3ac1086ed2914e747b3d06c01b1219457eab25
rishiraj007 <rishiraj@mounttalent.com>
2023-07-11 15:09:22 +0530

    [Prabhu]Add: Find the nearest number

---
+++ ReflectionPracticeProblem/NearestNumber.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionPracticeProblem
+{
+    public class NearestNumber
+    {
+        public void FindClosestNumberWithEvenDigits(int N)
+        {
+            if (N >= 0)
+            {
+                FindClosestNumberWithEvenDigitsPositive(N);
+            }
+            else
+            {
+                FindClosestNumberWithEvenDigitsNegative(N);
+            }
+        }
+
+        public int FindClosestNumberWithEvenDigitsPositive(int N)
+        {
+            int current = N;
+            while (true)
+            {
+                if (ContainsOnlyEvenDigits(current))
+                {
+                    return current;
+                }
+                current++;
+            }
+        }
+
+        public int FindClosestNumberWithEvenDigitsNegative(int N)
+        {
+            int current = N;
+            while (true)
+            {
+                if (ContainsOnlyEvenDigits(current))
+                {
+                    return current;
+                }
+                current--;
+            }
+        }
+
+        public bool ContainsOnlyEvenDigits(int number)
+        {
+            string digits = Math.Abs(number).ToString();
+            foreach (char digit in digits)
+            {
+                int digitValue = int.Parse(digit.ToString());
+                if (digitValue % 2 != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- ReflectionPracticeProblem/Program.cs
+++ ReflectionPracticeProblem/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            NearestNumber nearestNumber = new NearestNumber();
             Console.WriteLine("Reflection Practice Problem!");
+            Console.WriteLine("Enter any number");
+            int N = int.Parse(Console.ReadLine());
+            int result = nearestNumber.FindClosestNumberWithEvenDigitsPositive(N);
+            Console.WriteLine(result);
         }
     }
 }
